9487a370c7b249b64e45d01afa51e6b7e8c58d57
Prem Kumar <205642704+Kali-Prem@users.noreply.github.com>
2026-05-05 12:17:09 +0530

    Create 2.Operators.py

---
+++ 01.Python_Basics/1.Fundamentals-01/2.Operators.py
@@ -0,0 +1,50 @@
+# Arithmetic Operators
+a = 10
+b = 5
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b) #gives the remainder
+print(a%b) #gives the last digit
+print(a**b) #astrec is used to make the power a to the power b
+
+
+# Relational/Comparison Operator
+
+'''
+>, >=, <, <=, ==, !=
+This gives true or false value
+'''
+print(a>b)
+print(a<=b)
+print(a==b)
+
+
+# ========Assignment Variable===
+'''
+= , += , *= , /= , %=
+
+'''
+a += 5; #a = a + 5
+print(a)
+
+
+
+
+# ========Logical Operators======
+'''
+not - reverse the value 
+and - gives true when two values are true
+or  - gives true when one value is true
+
+'''
+
+print(not (a>b)) #false
+print(not a==b)  #true
+
+
+print((a>b) and (a==b)) #false
+
+print((a>b) or (a==b))  #true
+
