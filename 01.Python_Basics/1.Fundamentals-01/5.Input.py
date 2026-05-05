011272843abe102c72657d6d6e16bfc4c8a41127
Prem Kumar <205642704+Kali-Prem@users.noreply.github.com>
2026-05-05 12:17:26 +0530

    Create 5.Input.py

---
+++ 01.Python_Basics/1.Fundamentals-01/5.Input.py
@@ -0,0 +1,23 @@
+# 
+a = input("enter value of a:")
+print(a)
+
+# 
+username = input("Enter your username")
+print("welcome ", username)
+
+
+
+# ==================================================
+a = input("Enter the value of a: ") #10 -> input -> "10"
+b = input("Enter value of b: ")     # so here b = "5"
+
+sum = a + b   #sum = 105
+print(sum) #105
+# NOTE:-input function store all inputs like string
+
+#if we want to add then we have to change the input datatype
+a = int(input("Enter value of a: "))
+b = int(input("Enter value of b: "))
+sum = a + b
+print(sum)
