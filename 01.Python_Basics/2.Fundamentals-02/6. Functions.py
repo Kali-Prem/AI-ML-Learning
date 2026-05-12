13b6f820e5a00f0dda598c8bdc25b9b5fbaf77ab
Prem Kumar <205642704+Kali-Prem@users.noreply.github.com>
2026-05-12 15:16:10 +0530

    Create 6. Functions.py

---
+++ 01.Python_Basics/2.Fundamentals-02/6. Functions.py
@@ -0,0 +1,11 @@
+# functions are the block of codes that perform a specific task
+# Syntax: 
+# def fnxname();
+
+def hello(): #fnx definition
+    print("hello")
+    print("from python")
+# call the functions
+
+hello()
+hello()
