01c2576caf7f665b249213b38dc2d9c83cc52aa0
Prem Kumar <205642704+Kali-Prem@users.noreply.github.com>
2026-05-13 12:30:09 +0530

    Update 5. ForLoop.py

--- 01.Python_Basics/2.Fundamentals-02/5. ForLoop.py
+++ 01.Python_Basics/2.Fundamentals-02/5. ForLoop.py
@@ -1,4 +1,8 @@
+# For Loop syntax: 
+#   for i in range(start,end,iteration):
+
 string = "hello"
+
 
 #in => membership operator
 
@@ -40,7 +44,64 @@ print(count)
 
 
 
-# ------------
+# ------------1. Print numbers from 1 to 10---------------
+for i in range(1,11):    #range start from 0 . so i add 1 in the print function
+    print(i)
 
+
+# -------------2. Print even numbers from 1 to 50------
+
+for i in range(2,51, 2):
+    print(i)
+
+# ------------3. Sum of first N natural numbers----
+n = int(input("enter your number :"))
+sum = 0
+for i in range(1,n):
+    sum = sum + i
+print(sum)
+
+
+# ---------4. Multiplication table------
+n = int(input("enter the number:"))
+for i in range(1,11):
+    print(i*n)
+
+
+
+
+# ---------5. Factorial of a number-----------
+num = int(input("enter your number: "))
+factorial = 1
+for i in range(1,num+1):
+    factorial = factorial*i
+print(factorial)
+
+
+# --------6. Reverse a string----------
+word = input("enter th word: ") #word = "hello"
+reverse = ""
+for ch in word:
+    reverse = ch + reverse
+print(reverse)
+
+
+
+# ---------7. Count vowels in a string--------
+word = input("Enter Word for vowel count: ")
+word.lower()
+print(word.lower())
+count = 0
+for ch in word:
+    if ch in "aeiou":
+        count += 1
+
+    # if(ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u'):
+    #     count += 1
+print(count)
+
+
+
+# ---------
 
 
